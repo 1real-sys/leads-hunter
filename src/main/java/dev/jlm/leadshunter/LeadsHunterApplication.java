c6701975d1d614d49036deedd8f5f7088d6b2596
1real-sys <cauan.anna@edu.ufes.br>
2026-08-04 22:22:55 -0300

    first commit

---
+++ src/main/java/dev/jlm/leadshunter/LeadsHunterApplication.java
@@ -0,0 +1,13 @@
+package dev.jlm.leadshunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeadsHunterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jlm/leadshunter/LeadsHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jlm.leadshunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsHunterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
